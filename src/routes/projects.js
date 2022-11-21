95f8d18c96a1c8816c2ba35364e348e6acb78986
Princess Washington <princessw027@gmail.com>
2022-11-21 12:29:22 +0000

    Adding links

---
+++ src/routes/projects.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Navbar } from "../components/Navbar";
+
+export default function Projects(){
+    return(
+        <>
+        <Navbar/>
+        <div>
+        <h1>
+        This is our Projects Page.
+        </h1>
+        </div>
+        </>
+    )
+}
